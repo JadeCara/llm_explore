1328f78362e8e388fec798e628d13c0525339e75
Jade Wibbels <jade@Jades-MBP.local>
2024-10-15 15:29:30 -0600

    added summary notebook

--- summary/bart.py
+++ summary/bart.py
@@ -17,21 +17,3 @@ def summarize_document_with_bart(text, model_name="facebook/bart-large-cnn"):
 
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
-
-
-if __name__ == "__main__":
-    document_text = (
-        """Artificial intelligence (AI) is intelligence demonstrated by machines, """
-        """in contrast to the natural intelligence displayed by humans and animals."""
-        """Leading AI textbooks define the field as the study of "intelligent agents": """
-        """any device that perceives its environment and takes actions that maximize"""
-        """its chance of successfully achieving its goals. Colloquially, the term """
-        """"artificial intelligence" is often used to describe machines (or computers)"""
-        """that mimic "cognitive" functions that humans associate with the human mind, """
-        """such as "learning" and "problem solving"."""
-    )
-
-    summary = summarize_document_with_bart(document_text)
-
-    print("Summary:")
-    print(summary)
